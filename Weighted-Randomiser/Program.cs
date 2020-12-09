70685a8503cdc7fcb97db9eff5b29df63a004e4f
John Michael Stock <ziplock9000@msn.com>
2020-12-09 06:13:49 +0000

    Initial working version

---
+++ Weighted-Randomiser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Weighted_Randomiser
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Create a list of items that can be randomly selected with weights
+            WeightedRandomiser<string> itemDrops = new WeightedRandomiser<string>();
+
+            itemDrops.AddEntry("10 Gold", 1.0);
+            itemDrops.AddEntry("Sword", 20.0);
+            itemDrops.AddEntry("Shield", 50.0);
+            itemDrops.AddEntry("Armor", 20.0);
+            itemDrops.AddEntry("Potion", 10.0);
+
+            //Pick some random items
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"Item {i} = {itemDrops.GetRandom()}");
+            }
+
+            Console.WriteLine("Hit Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Weighted-Randomiser/Weighted-Randomiser.cs
@@ -0,0 +1,39 @@
+﻿//
+// Code originally from Philipp @ https://gamedev.stackexchange.com/questions/162976/how-do-i-create-a-weighted-collection-and-then-pick-a-random-element-from-it
+//
+using System;
+using System.Collections.Generic;
+
+internal class WeightedRandomiser<T>
+{
+
+    private struct Entry
+    {
+        public double accumulatedWeight;
+        public T item;
+    }
+
+    private readonly List<Entry> entries = new List<Entry>();
+    private double accumulatedWeight;
+    private readonly Random rand = new Random();
+
+    public void AddEntry(T item, double weight)
+    {
+        accumulatedWeight += weight;
+        entries.Add(new Entry { item = item, accumulatedWeight = accumulatedWeight });
+    }
+
+    public T GetRandom()
+    {
+        double r = rand.NextDouble() * accumulatedWeight;
+
+        foreach (Entry entry in entries)
+        {
+            if (entry.accumulatedWeight >= r)
+            {
+                return entry.item;
+            }
+        }
+        return default(T);
+    }
+}
